f9294750c7c98faed23f87ae62736fdfffc2eabb
Christopher Evans <christopher.evans@mediamonks.com>
2021-07-13 10:47:55 +0200

    Fix typo

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ export default function App() {
   return (
     <>
       <Header />
-      <div className='container'>
-        <div className='title'>
-          <h1>Stater Branch</h1>
+      <div className="container">
+        <div className="title">
+          <h1>Starter Branch</h1>
         </div>
       </div>
     </>
